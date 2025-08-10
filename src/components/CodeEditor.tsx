01e83afcdaf4b2694221b1bc116c1cffe2541ba0
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2025-08-10 16:57:51 +0700

    Update CodeEditor.tsx

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import Editor, { type OnMount } from '@monaco-editor/react';
 import { useScrimbaContext } from '../hooks/useScrimbaContext';
 
