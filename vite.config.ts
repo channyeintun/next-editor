afae1a9a9513c99ba2aad73672f7b2b393b5e0bf
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2026-01-23 14:40:08 +0700

    feat: migrate to vite 8 and native rolldown integration with robust minification

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,30 @@ export default defineConfig({
   ],
   build: {
     minify: true,
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: 'vendor',
+              test: /[\\/]node_modules[\\/](react|react-dom|react-router-dom)[\\/]/,
+            },
+            {
+              name: 'editor',
+              test: /[\\/]node_modules[\\/](@monaco-editor|monaco-editor)[\\/]/,
+            },
+            {
+              name: 'xstate',
+              test: /[\\/]node_modules[\\/](xstate|@xstate\/react)[\\/]/,
+            },
+            {
+              name: 'utils',
+              test: /[\\/]node_modules[\\/](pako|superjson)[\\/]/,
+            },
+          ],
+        },
+      },
+    },
     chunkSizeWarningLimit: 1024,
     reportCompressedSize: false,
     sourcemap: false,
